5227119057bdd39dde6d2d76af0b190172a68bfa
Your Name <you@example.com>
2017-04-23 13:38:06 -0400

    Added get request

--- app/src/main/java/com/example/mushfiqkhan/flynntodoapp/MainActivity.java
+++ app/src/main/java/com/example/mushfiqkhan/flynntodoapp/MainActivity.java
@@ -2,16 +2,30 @@ package com.example.mushfiqkhan.flynntodoapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import static android.R.id.content;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,11 +49,73 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        
+
         adapter = new ArrayAdapter<String>(this,
                 R.layout.activity_listview, listItems);
         ListView listView = (ListView) findViewById(R.id.mobile_list);
         listView.setAdapter(adapter);
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    URL url = null;
+                    HttpURLConnection urlConnection = null;
+                    try {
+                        url = new URL("http://jsonplaceholder.typicode.com/todos");
+                        urlConnection = (HttpURLConnection) url.openConnection();
+                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                        int responseCode = urlConnection.getResponseCode();
+
+                        if(responseCode == HttpURLConnection.HTTP_OK){
+                            String server_response = readStream(urlConnection.getInputStream());
+                            JSONArray jsonArray = new JSONArray(server_response);
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject explrObject = jsonArray.getJSONObject(i);
+                                listItems.add(explrObject.getString("title"));
+                            }
+                            adapter.notifyDataSetChanged();
+                        }
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    finally {
+                        urlConnection.disconnect();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+    }
+
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
     }
 
     @Override
